5ce2d56dae873a30aefe3204727d7223861cdabb
SS <kkotkkio@gmail.com>
2015-04-15 04:51:02 +0900

    # ajax-form 모듈, 서버연동완료 (수정,삭제,패스워드검증,메인슬라이드오류 미해결)

--- dist/js/common.js
+++ dist/js/common.js
@@ -1,2 +1,30 @@
-var __TARTAR_HOST__ = "http://localhost:8080";
+var __TARTAR_HOST__ = "http://ec2-54-65-110-115.ap-northeast-1.compute.amazonaws.com";
 
+function chkInput(obj, msg){
+    if(obj.value == ""){
+        alert(msg);
+        obj.focus();
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function frmChkValue(frm){
+
+    switch(frm.id){
+        case "contactFrm" :
+            if(!chkInput(frm.contact_name, "이름을 입력해주세요 ~ ")) return false;
+            if(!chkInput(frm.contact_email, "이메일을 입력해주세요 ~")) return false;
+            if(!chkInput(frm.message_content, "내용을 입력 해주세요 ~ ")) return false;
+        break;
+        case "boardWriteFrm" :
+            if(!chkInput(frm.subject, "제목을 입력해주세요 ~ ")) return false;
+            if(!chkInput(frm.content, "내용을 입력해주세요 ~")) return false;
+            if(!chkInput(frm.name, "이름을 입력 해주세요 ~ ")) return false;
+            if(!chkInput(frm.password, "비밀번호를 입력 해주세요 ~ ")) return false;
+        break;
+    }
+
+    return true;
+}
